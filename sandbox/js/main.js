8f8e0b7dcb40ea918d14669d6b3151eff5a0718e
ornsio <kkwillmert@gmail.com>
2021-10-07 09:41:58 -0400

    Add some comments

--- sandbox/js/main.js
+++ sandbox/js/main.js
@@ -1,10 +1,13 @@
 if ( window.Worker ) {
+    // create the 'worker' variable to interact with the worker from the main thread
     const worker = new Worker( 'js/worker.js' );
 
+    // establish logic to handle messages sent from the worker to the main thread
     worker.onmessage = function( event ) {
         console.log( 'Worker sent main thread a message: ' + event.data );
     }
 
+    // logic to send messages from the main thread to the worker
     function buttonClick() {
         worker.postMessage( 'This is a message from the main thread.' );
         console.log( 'Main thread sent worker a message.' );
--- sandbox/js/worker.js
+++ sandbox/js/worker.js
@@ -1,3 +1,4 @@
+// handle messages from the main thread
 onmessage = function( event ) {
     console.log( 'Worker: Message received from main thread: ' + event.data );
     postMessage( 'Hey main thread, I got your message.' );
