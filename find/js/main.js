c3332f699260d507b102437e74a08abba90a5af1
ornsio <kkwillmert@gmail.com>
2021-11-29 14:47:44 -0500

    Misc. improvements to find.

--- find/js/main.js
+++ find/js/main.js
@@ -1,10 +1,21 @@
+//
+//
+// TODO:
+//   - Color line numbers differently when they are adjacent to filtered-out lines
+//   - Add different search options
+//   - Get horizontal scrolling to leave line numbers in-place (see https://stackoverflow.com/questions/51299387/fix-items-only-horizontally-or-vertically-in-the-same-scroll-container/51299660)
+//
+//
 var controls;
 var userFile;
 var userText;
 var parseButton;
+var toggleInputs;
+var hideable;
 var searchBox;
 var unfiltered;
 var filtered;
+var loading;
 
 var userTextReady = false;
 var searchDelayDefault = 2;
@@ -12,6 +23,7 @@ var searchDelay = searchDelayDefault;
 var searchWaitInProgress = false;
 var searchInProgress = false;
 var waitingOn = 0;
+var lastSearch = '';
 
 var workerA;
 var workerB;
@@ -23,17 +35,24 @@ window.addEventListener( 'load', function() {
         alert( "Your browser doesn't support web workers, so this page won't work for you :-(" );
     }
     else {
-        controls    = document.getElementById( 'controls' );
-        userFile    = document.getElementById( 'userFile' );
-        userText    = document.getElementById( 'userText' );
-        parseButton = document.getElementById( 'parseButton' );
-        searchBox   = document.getElementById( 'search' );
-        unfiltered  = document.getElementById( 'unfiltered' );
-        filtered    = document.getElementById( 'filtered' );
+        controls     = document.getElementById( 'controls' );
+        userFile     = document.getElementById( 'userFile' );
+        userText     = document.getElementById( 'userText' );
+        parseButton  = document.getElementById( 'parseButton' );
+        toggleInputs = document.getElementById( 'toggleInputs' );
+        hideable     = document.getElementById( 'hideable' );
+        searchBox    = document.getElementById( 'search' );
+        unfiltered   = document.getElementById( 'unfiltered' );
+        filtered     = document.getElementById( 'filtered' );
+        loading      = document.getElementById( 'loadingImage' );
 
         setResultHeight();
         window.addEventListener( 'resize', setResultHeight );
         new MutationObserver( setResultHeight ).observe( userText, {
+            attributes: true,
+            attributeFilter: [ "style" ]
+        });
+        new MutationObserver( setResultHeight ).observe( hideable, {
             attributes: true,
             attributeFilter: [ "style" ]
         });
@@ -49,11 +68,11 @@ window.addEventListener( 'load', function() {
         
         // Establish logic to handle messages sent from the worker threads to the main thread
         var workerResponseHandler = function( event ) {
-            console.log( 'Worker ' + event.data.worker + ' sent main thread a message : ' + JSON.stringify( event.data ) );
+            // console.log( 'Worker ' + event.data.worker + ' sent main thread a message : ' + JSON.stringify( event.data ) );
             var row = document.getElementById( event.data.id );
 
             if ( event.data.rowMatched ) {
-                row.innerHTML = event.data.text;
+                row.children[1].innerHTML = event.data.text;
                 row.style.display = 'block';
             }
             else {
@@ -90,16 +109,23 @@ window.addEventListener( 'load', function() {
 function parseTextbox()
 {
     preParse();
-    var textArray = userText.value.split( '\n' )
-    parse( textArray );
+    // Have to briefly delay before doing the work, otherwise the loading graphic and other visual changes won't render first
+    setTimeout( function() {
+        var textArray = userText.value.split( '\n' );
+        parse( textArray );
+    },
+    100 );
 }
 
 function parseFile() {
     if ( !userFile.disabled ) {
         preParse();
-
-        const file = userFile.files[0];
-        file.text().then( text => parse( text.split( '\n' ) ) );
+        // Have to briefly delay before doing the work, otherwise the loading graphic and other visual changes won't render first
+        setTimeout( function() {
+            const file = userFile.files[0];
+            file.text().then( text => parse( text.split( '\n' ) ) );
+        },
+        100 );
     }
 }
 
@@ -107,11 +133,10 @@ function preParse() {
     userFile.disabled    = true;
     userText.disabled    = true;
     parseButton.disabled = true;
+    loading.style.display = 'block';
 
     unfiltered.textContent = '';
     filtered.textContent = '';
-
-    // Probably need to display some kind of "loading" graphic
 }
 
 function parse( textArray ) {
@@ -120,15 +145,36 @@ function parse( textArray ) {
         var line = textArray[i];
 
         var newLine = document.createElement( 'div' );
-        newLine.innerText = line;
         newLine.id = 'u' + ( i + 1 );
         newLine.className = 'line';
+
+        var lineNum = document.createElement( 'div' );
+        lineNum.innerText = i + 1;
+        lineNum.className = 'lineNum';
+
+        var lineText = document.createElement( 'div' );
+        lineText.innerText = line;
+        lineText.className = 'lineText';
+
+        newLine.appendChild( lineNum );
+        newLine.appendChild( lineText );
         unfiltered.appendChild( newLine );
 
+        
         var newLineFiltered = document.createElement( 'div' );
-        // newLineFiltered.innerText = line; // no need to have the text in the filtered section initially
         newLineFiltered.id = i + 1;
         newLineFiltered.className = 'line';
+
+        var lineNumF = document.createElement( 'div' );
+        lineNumF.innerText = i + 1;
+        lineNumF.className = 'lineNum';
+
+        var lineTextF = document.createElement( 'div' );
+        // lineTextF.innerText = line;  // no need to include the text on the filtered lines initially since worker responses will populate it later
+        lineTextF.className = 'lineText';
+
+        newLineFiltered.appendChild( lineNumF );
+        newLineFiltered.appendChild( lineTextF );
         filtered.appendChild( newLineFiltered );
     }
 
@@ -136,6 +182,24 @@ function parse( textArray ) {
     searchBox.disabled = false;
     searchBox.focus();
     unfiltered.style.display = 'block';
+    loading.style.display = 'none';
+
+    toggleInputs.addEventListener( 'click', toggle );
+    toggleInputs.style.display = 'block';
+    toggle();
+}
+
+
+function toggle() {
+    if ( hideable.style.display == 'none' ) {
+        hideable.style.display = 'block';
+        toggleInputs.innerText = 'Hide Inputs';
+    }
+    else {
+        hideable.style.display = 'none';
+        toggleInputs.innerText = 'Show Inputs';
+    }
+    searchBox.focus();
 }
 
 
@@ -143,7 +207,15 @@ function searchCheck() {
     searchDelay--;
 
     if ( searchDelay <= 0 ) {
-        search();
+        searchWaitInProgress = false;
+
+        if ( searchBox.value != lastSearch ) {
+            searchInProgress = true;
+            loading.style.display = 'block';
+            searchBox.disabled = true;
+            // Have to briefly delay before doing the work, otherwise the loading graphic and other visual changes won't render first
+            setTimeout( search, 100 );
+        }
     }
     else {
         setTimeout( searchCheck, 1000 );
@@ -151,15 +223,12 @@ function searchCheck() {
 }
 
 function search() {
-    searchInProgress = true;
-    searchWaitInProgress = false;
-    
+    var searchStart = ( new Date() ).getTime();
+
     if ( searchBox.value !== '' ) {
         unfiltered.style.display = 'none';
         filtered.style.display = 'block';
-
         waitingOn = 0;
-        searchBox.disabled = true;
 
         var children = unfiltered.children;
 
@@ -167,11 +236,12 @@ function search() {
             var line = children[i];
 
             // Implement search options here to alter what regex is used
+            lastSearch = searchBox.value;
             var searchInput = searchBox.value.replaceAll( /[\^\$\(\)\<\>\[\]\{\}\|\.\*\+\?\\]/g, '\\$&' );
             var matcher = new RegExp( searchInput, 'ig' );
 
-            var message = { id : line.id.substring( 1 ), text : line.innerText, searchRegEx : matcher };
-            console.log( 'Main thread sending worker a message : ' + JSON.stringify( message ) );
+            var message = { id : line.id.substring( 1 ), text : line.children[1].innerText, searchRegEx : matcher };
+            // console.log( 'Main thread sending worker a message : ' + JSON.stringify( message ) );
             waitingOn++;
 
             var whichWorker = i % numWorkers;
@@ -194,13 +264,30 @@ function search() {
     else {
         unfiltered.style.display = 'block';
         filtered.style.display = 'none';
+        searchBox.disabled = false;
+        searchBox.focus();
     }
 
     searchInProgress = false;
+    loading.style.display = 'none';
+    searchBox.placeholder = ' type your search here...';
+
+    var searchTime = ( new Date() ).getTime() - searchStart;
+    var searchMils    = 0;
+    var searchSeconds = 0;
+    var searchMinutes = 0;
+    if ( searchTime > 0 ) {
+        searchMils    = searchTime % 1000;
+        searchSeconds = ( searchTime - searchMils ) / 1000;
+        searchTime    = searchSeconds;
+        searchSeconds = searchTime % 60
+        searchMinutes = ( searchTime - searchSeconds ) / 60;
+    }
+    console.log( 'Search time - ' + searchMinutes + ':' + searchSeconds + '.' + searchMils );
 }
 
 function setResultHeight() {
-    var resultHeight = ( window.innerHeight - controls.getBoundingClientRect().height - 130 ) + 'px';
+    var resultHeight = ( window.innerHeight - controls.getBoundingClientRect().height - 80 ) + 'px';
     unfiltered.style.height = resultHeight;
     filtered.style.height = resultHeight;
 }
