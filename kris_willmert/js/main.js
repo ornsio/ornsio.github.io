322e30fe65a55dac3c58cd642ca97abe8a0aaf42
ornsio <kkwillmert@gmail.com>
2023-01-20 16:31:18 -0500

    Temporarily make Ellucian emphasis the default

--- kris_willmert/js/main.js
+++ kris_willmert/js/main.js
@@ -1,7 +1,7 @@
-const qsp = new URLSearchParams( window.location.search );
+// const qsp = new URLSearchParams( window.location.search );
 
-if ( qsp.has( "emphasis" ) ) {
-    const emph = qsp.get( "emphasis" );
+// if ( qsp.has( "emphasis" ) ) {
+    // const emph = qsp.get( "emphasis" );
 
     let defaultPrintNote   = document.getElementById( "printNoteDefault" );
     let defaultExpButton   = document.getElementById( "expButton4" );
@@ -13,21 +13,21 @@ if ( qsp.has( "emphasis" ) ) {
     let ellucianExpContent = document.getElementById( "expContent2" );
     let ellucianSkills     = document.getElementById( "skillsEllucian" );
 
-    switch ( emph ) {
-        case "ellucian":
-            defaultPrintNote.classList.add( "d-none" );
+    // switch ( emph ) {
+        // case "ellucian":
+            // defaultPrintNote.classList.add( "d-none" );
             defaultExpButton.classList.add( "collapsed" );
             defaultExpContent.classList.remove( "show" );
             defaultSkills.classList.add( "d-none" );
 
-            ellucianPrintNote.classList.remove( "d-none" );
+            // ellucianPrintNote.classList.remove( "d-none" );
             ellucianExpButton.classList.remove( "collapsed" );
             ellucianExpContent.classList.add( "show" );
             ellucianSkills.classList.remove( "d-none" );
 
-            break;
-    }
-}
+            // break;
+    // }
+// }
 
 $.get("./si/1.html",function(data){$('#item1').html(atob(data))},);
 $.get("./si/2.html",function(data){$('#item2').html(atob(data))},);
