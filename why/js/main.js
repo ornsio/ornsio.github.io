5e6a9b527d71960608abfd66336ca165ad98c8f1
ornsio <kkwillmert@gmail.com>
2026-05-01 12:46:46 -0400

    why: add scroll event to opened accordion headers

--- why/js/main.js
+++ why/js/main.js
@@ -3,3 +3,16 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+document.querySelectorAll('.accordion-collapse').forEach(collapse => {
+    collapse.addEventListener('shown.bs.collapse', function (event) {
+        // Only act if THIS collapse element is the one that fired the event
+        if (event.target !== this) return;
+
+        const header = this.previousElementSibling;
+            header.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
